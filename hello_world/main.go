5e3dcc16c6bdef8efd56f10882013165254e85cc
Gabriel Costa <gabriel.costa@blitzpay.com.br>
2026-01-02 12:39:24 -0300

    feat: Add more scripts

--- hello_world/main.go
+++ hello_world/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func say(s string, done chan string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+	done <- "Terminei"
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	// start := time.Now()
+	// fmt.Println("Hello World!")
+	// secs := time.Since(start).Seconds()
+	// fmt.Printf("%.2fs \n", secs)
+	done := make(chan string)
+	go say("world", done)
+	go say("hello", done)
+	fmt.Println(<-done)
 }
---
+++ interfaces/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Shape interface {
+	Area()
+}
+
+type Rectangle struct {
+	width, heigth float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.width * r.heigth
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func calculateArea(s Shape) float64 {
+	return s.Area()
+}
+
+func main() {
+	rect := Rectangle{width: 5, heigth: 4}
+	circle := Circle{radius: 2}
+	fmt.Println(rect, circle)
+	// fmt.Println("Rectangle area is:", calculateArea(rect)) COMPILER ERROR
+	// fmt.Println("Rectangle area is:", calculateArea(circle)) COMPILER ERROR
+}
